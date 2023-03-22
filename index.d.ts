83f95c08f231ed7cf7bcf955d074c7396bb75638
TirthaAhmadNazuha <tirthaahmadnazuha.udah@gmail.com>
2023-03-22 22:48:12 +0700

    change index.js to index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+import AmberJsx from './amber-jsx'
+import BaseComponent from './component/base'
+import SyncComponent from './component/async'
+
+export { BaseComponent, SyncComponent, AmberJsx as default }
