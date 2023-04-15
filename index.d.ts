aeb59aff4894aa94cb78854232c49ee180f6ee20
TirthaAhmadNazuha <tirthaahmadnazuha.udah@gmail.com>
2023-04-15 22:25:40 +0700

    try add syntax in index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,14 @@
-declare module 'amber';
+declare module 'amber' {
+  declare type Test = {
+    get: (name: string) => any,
+    set: (name: string, value: any) => any,
+    has: (name: string) => boolean,
+    delete: (name: string) => any,
+    keys: () => Array<string>,
+    values: () => Array<any>,
+    getProperties: () => Array<string>,
+    getProperty: (name: string) => any,
+    setProperty: (name: string, value: any) => any,
+    getPropertyNames: () => Array<string>,
+  }
+}
